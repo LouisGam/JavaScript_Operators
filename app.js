f4c1270abc7037965f780bd88d662c82f8bee1da
Louis Gambardella <LouisGambardella@gmail.com>
2024-03-29 10:17:41 -0700

    Excsersie

--- app.js
+++ app.js
@@ -17,3 +17,55 @@ console.log("EXERCISE 2:\n==========\n");
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+// JavaScript Operators
+// Excerise 1
+let a=20;
+let b=4;
+let add=a+b;
+let minus=a-b;
+let divividing=a/b;
+
+// Excerise 2
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+//
+ //num + str?//
+ //num + str2?//
+ //num + isPresent?//
+ //num +firstName + num?//
+ //isPresent + str?//
+ //What is the value of: firstName + lastName?//
+console.log(num+str); // 1111 //
+console.log(firstName+num);//Frodo11//
+console.log(firstName+lastName);//FrodoBaggins
+
+//Excersise 3
+
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+ /*
+What is the value of: val == str3?
+5=="5" true
+What is the value of: val === str3?
+5==="5" false
+What is the value of: !isPresent2?
+!false ->True
+What is the value of: (“eleven” == str4 && val >= str3)?
+->false
+What is the value of: (!isPresent2 || isPresent2)?
+->
+What is the value of: 0 == false?
+->true
+What is the value of: 0 === false?
+->false
+What is the value of: 0 != false?
+false
+What is the value of 0 !== false?
+true
+ */
